dd4e18f385951afd87a9da475b3d68e3bc4bc413
Mikael Olsson <mikael.olsson@emmio.se>
2020-09-24 10:04:20 +0200

    Added some comments

--- js/game.js
+++ js/game.js
@@ -4,16 +4,20 @@ class Game {
     }
 
     partSum() {
+        // Loop through the players
         for (let player of this.players) {
+            // Get the relevant components
             let fields = document.querySelectorAll(".player" + player + ".part_sum");
             let sum = 0;
 
+            // Add all field values to the sum
             for (let field of fields) {
                 if (!isNaN(Number(field.valueAsNumber))) {
                     sum += Number(field.valueAsNumber);
                 }
             }
 
+            // Update the sum in the form
             let partsum_field = document.querySelector(".sum1 .player" + player);
             partsum_field.innerHTML = sum;
         }
